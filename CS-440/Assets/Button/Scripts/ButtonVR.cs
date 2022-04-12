fab9f6323d253144da4405d245f444cf287ee458
Gianniii <gianni.lodetti@hotmail.com>
2022-04-12 13:15:12 +0200

    Number keyboard - Not finished yet

---
+++ CS-440/Assets/Button/Scripts/ButtonVR.cs
@@ -0,0 +1,56 @@
+/**************************************************
+Copyright : Copyright (c) RealaryVR. All rights reserved.
+Description: Script for VR Button functionality.
+***************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonVR : MonoBehaviour
+{
+    public GameObject button;
+    public UnityEvent onPress;
+    public UnityEvent onRelease;
+    GameObject presser;
+    AudioSource sound;
+    bool isPressed;
+
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+        isPressed = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isPressed)
+        {
+            button.transform.localPosition = new Vector3(0, 0.003f, 0);
+            presser = other.gameObject;
+            onPress.Invoke();
+            sound.Play();
+            isPressed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == presser)
+        {
+            button.transform.localPosition = new Vector3(0, 0.015f, 0);
+            onRelease.Invoke();
+            isPressed = false;
+        }
+    }
+
+    public void SpawnSphere()
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        sphere.transform.localPosition = new Vector3(0, 1, 2);
+        sphere.AddComponent<Rigidbody>();
+    }
+
+}
