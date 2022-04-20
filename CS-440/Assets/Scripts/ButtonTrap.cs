e79a16f63cf0f851c0c92698397a017c2bd3db8f
Olivier Lonneux <79085071+Olivier-Lonneux@users.noreply.github.com>
2022-04-20 19:37:11 +0200

    Button open trap updated and added to rocketElia

--- CS-440/Assets/Scripts/ButtonTrap.cs
+++ CS-440/Assets/Scripts/ButtonTrap.cs
@@ -5,17 +5,18 @@ using UnityEngine.Events;
 
 public class ButtonTrap : MonoBehaviour
 {
-   
-   public GameObject button;
-   public UnityEvent onPress;
-   public UnityEvent onRelease;
-   GameObject presser;
-   AudioSource sound;
-   bool isPressed;
+    [SerializeField]
+    public GameObject Door;
+    public GameObject button;
+    private Animator anim;
+    GameObject presser;
+    AudioSource sound;
+    bool isPressed;
     void Start()
     {
         sound = GetComponent<AudioSource>();
         isPressed = false;
+        anim = Door.GetComponent<Animator>();
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -23,7 +24,7 @@ public class ButtonTrap : MonoBehaviour
             button.transform.position = button.transform.position + new Vector3(0, -0.02f, 0);
             other.transform.position = other.transform.position + new Vector3(0, -0.02f, 0);
             presser = other.gameObject;
-            onPress.Invoke();
+            onButtonPressed();
             sound.Play();
             isPressed = true;
         }
@@ -32,9 +33,17 @@ public class ButtonTrap : MonoBehaviour
     private void OnTriggerExit(Collider other) {
         if(other.gameObject == presser) {
             button.transform.position = button.transform.position + new Vector3(0, 0.02f, 0);
-            onRelease.Invoke();
+            onButtonReleased();
             isPressed = false;
         }
+    }
+    private void onButtonPressed()
+    {
+        anim.SetBool("button_pressed", true);
+    }
+    private void onButtonReleased()
+    {
+        anim.SetBool("button_pressed", false);
     }
 }
 
