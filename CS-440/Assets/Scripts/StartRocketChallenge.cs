328f39086f307c2944066183e06e9bc0bcddab1d
Gianniii <gianni.lodetti@hotmail.com>
2022-05-11 22:15:39 +0200

    lever only works when first two riddles done

--- CS-440/Assets/Scripts/StartRocketChallenge.cs
+++ CS-440/Assets/Scripts/StartRocketChallenge.cs
@@ -12,10 +12,21 @@ public class StartRocketChallenge : MonoBehaviour
     public ButtonLamp buttonLamp2;
     public ButtonLamp tabletButtonLamp;
 
+    public JoystickControl joystickControl;
+    public ButtonLamp joystickActiveLight;
+
     // Update is called once per frame
     public ButtonLamp finalLamp;
     void Update()
     {   
+        //when two first challenger are done activate the joystick
+        if(buttonLamp2.on && buttonLamp2.lightColor == ButtonLamp.eColor.Green  
+        && tabletButtonLamp.on && tabletButtonLamp.lightColor == ButtonLamp.eColor.Green) {
+            joystickControl.canMove = true;
+            joystickActiveLight.on = true;
+            joystickActiveLight.lightColor = ButtonLamp.eColor.Green;
+
+        }
         if(buttonLamp1.on && buttonLamp1.lightColor == ButtonLamp.eColor.Green 
         && buttonLamp2.on && buttonLamp2.lightColor == ButtonLamp.eColor.Green 
         && tabletButtonLamp.on && tabletButtonLamp.lightColor == ButtonLamp.eColor.Green) {
