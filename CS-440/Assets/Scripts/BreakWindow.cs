c371239718ab2a44a3eff66da281460bc5fc8b21
Olivier Lonneux <79085071+Olivier-Lonneux@users.noreply.github.com>
2022-05-12 14:34:15 +0200

    fix breakable window hopefully

---
+++ CS-440/Assets/Scripts/BreakWindow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakWindow : MonoBehaviour {
+
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "hammer"){
+            gameObject.GetComponent<BreakableWindow>().breakWindow();
+        }
+    }
+}
